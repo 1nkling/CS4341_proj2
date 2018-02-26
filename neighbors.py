77c579b8d97787d957681e9aff8a543a91f914e4
Steven Huynh <stevenhuynh@ste.vin>
2018-02-25 23:18:56 -0500

    Added code to compute precision and recall for each class in neighbors.py Also added code in neighbors.py to output images for visualizations (although it is commented out)

--- neighbors.py
+++ neighbors.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import floor
 from sklearn.neighbors import KNeighborsClassifier
+from skimage import io
 
 images_as_matrices = np.load('images.npy')
 labels_as_integers = np.load('labels.npy')
@@ -157,6 +158,7 @@ for i in range(4,5):
     
     print('number of right classifications: {}'.format(number_of_right_classifications))
     print('number of wrong classifications: {}'.format(number_of_wrong_classifications))
+    print('accuracy: {}'.format(number_of_right_classifications/len(test_set_classifications)))
     
     #
     # Confusion Matrix for the Validation Set
@@ -176,6 +178,41 @@ for i in range(4,5):
         true_label = test_set['labels'][i]
         predicted_label = test_set_classifications[i]
         confusion_matrix[true_label][predicted_label] += 1
-    
+
+        #if (true_label == 4 and predicted_label == 1):
+        #    test_set_image_as_matrix = np.reshape(test_set['images'][i], (28, 28))
+        #    io.imsave('test_set_image_{}.bmp'.format(i), test_set_image_as_matrix)
+
     print(np.matrix(confusion_matrix))
     print()
+
+    for i in range(10):
+        true_positives = confusion_matrix[i][i]
+        false_positives = 0
+        for j in range(10):
+            if j != i:
+                false_positives += confusion_matrix[j][i]
+        false_negatives = 0
+        for j in range(10):
+            if j != i:
+                false_negatives += confusion_matrix[i][j]
+        true_negatives = len(test_set_classifications) - (true_positives + false_positives + false_negatives)
+
+        #print('number of true positives for {} is {}'.format(i, true_positives))
+        #print('number of false positives for {} is {}'.format(i, false_positives))
+        #print('number of false negatives for {} is {}'.format(i, false_negatives))
+        #print('number of true negatives for {} is {}'.format(i, true_negatives))
+        print('Precision for {} is {}'.format(i, true_positives/(true_positives + false_positives)))
+        print('Recall for {} is {}'.format(i, true_positives/(true_positives + false_negatives)))
+        print()
+
+    #neighbors = classifier.kneighbors([test_set['images'][775]], 4)
+
+    #print(neighbors[1][0])
+    #for neighbor_index in neighbors[1][0]:
+    #    print(training_set['labels'][neighbor_index])
+    #print(neighbors[0][0])
+
+    #for neighbor_index in neighbors[1][0]:
+    #    neighbor_image_as_matrix = np.reshape(training_set['images'][neighbor_index], (28,28))
+    #    io.imsave('neighbor_image_{}.bmp'.format(neighbor_index), neighbor_image_as_matrix)
